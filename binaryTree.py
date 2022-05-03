287e3c5ec67781e05760a7eaca94669db7834cce
SuRRiEs006 <43891244+SuRRiEs006@users.noreply.github.com>
2022-05-04 00:59:47 +0100

    bracket matching done in scrap

--- binaryTree.py
+++ binaryTree.py
@@ -3,97 +3,66 @@
 - find function (https://stackoverflow.com/questions/11122291/how-to-find-char-in-string-and-get-all-the-indexes)
 
 '''
+
 #######IMPORT######
 import copy
 
 #######CODE#######
 
-def find(s, ch):
-    return [i for i, ltr in enumerate(s) if ltr == ch]
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-class BinaryTree:
+class BinaryTree(object):
+    def __init__(self, root):
+      self.root = Node(root)
 
+class BinaryTreeCreator:
     def __init__(self, binaryTreeStr):
+      self.defualtOperators = ['/', '*', '+', '-']
+      self.acceptedNums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+      self.otherSymbol = ['(', ')']
+      self.allSubStrings = self.defualtOperators + self.acceptedNums + self.otherSymbol
+      #print(self.allSubStrings)
+      self.binaryTreeStr = str(binaryTreeStr.replace(' ', ''))
+      print("lol")
 
-        self.defualtOperators = ['/','*','+','-']
-        self.acceptedNums = ['1','2','3','4','5','6','7','8','9']
-        self.otherSymbol = ['(',')']
-        self.allSubStrings = self.defualtOperators+self.acceptedNums+self.otherSymbol
-        print(self.allSubStrings)
-        self.binaryTreeStr = str(binaryTreeStr.replace(' ', ''))
+    def levelsOfTree(self,inputAray):
+      bracketCount = 0
+      levelsOfTreeArray = []
+      
+      for i in range(0,len(inputAray)):
 
-    def validSubStrings(self):
-        currentStr = copy.copy(self.binaryTreeStr)
-        isValidSubStrings = None
-        for x in range(0,len(self.allSubStrings)):
-
-            currentStr = currentStr.replace(self.allSubStrings[x], '')
-            if currentStr == '':
-                isValidSubStrings = True
-            else:
-                isValidSubStrings = False
-                
-        return(isValidSubStrings)
-
-    def validBrackets(self):
-        isValidBrackets = None
-        currentStr = copy.copy(self.binaryTreeStr)
-        openBracket = currentStr.count("(")
-        closeBracket = currentStr.count(")")
-
-        if openBracket != closeBracket:
-            isValidBrackets = False
-        else:
-            isValidBrackets = True
+        if inputAray[i] == "(" :
+          bracketCount += 1
+        elif inputAray[i] == ")" :
+          bracketCount -= 1
         
-        return(isValidBrackets)
+        if inputAray[i] in self.defualtOperators:
+          levelsOfTreeArray.append([inputAray[i],bracketCount,i])
 
-    def validOperand(self):
-        pass
-
-
-    def validateString(self):
-        print(12)
-        currentStr = copy.copy(self.binaryTreeStr)
-
-        isValidSubStrings = self.validSubStrings()
-        isValidBrackets = self.validBrackets()
-
-        # for i in range(0,len(self.defualtOperators)):
-        #     currentStr = currentStr.replace(self.defualtOperators[i], '.')
-
-        # operators = currentStr.count(".")
-        
-        if isValidBrackets == False:
-            print("Not a valid expression, bracket mismatched")
-
-        print(isValidSubStrings, isValidBrackets, currentStr)
-
-        print(self.arrayFormation())
+      return(levelsOfTreeArray)
     
-    def arrayFormation(self):
-        preCurrentStr = copy.copy(self.binaryTreeStr)
-
-        currentArray = [char for char in preCurrentStr]
-        openBracketIndex = find(preCurrentStr, '(')
-        closeBracketIndex = find(preCurrentStr, ')')
-
-        newArray = []
-        for p in range(0,len(openBracketIndex)):
-            
-            endOfBracket = closeBracketIndex[(len(openBracketIndex)-1)-p]
-            startOfBracket = openBracketIndex[p]
-
-            newArray = newArray + [currentArray[startOfBracket:endOfBracket]]
-            
-            print(newArray)
-        print(openBracketIndex,closeBracketIndex)
-        
-        
-
-    def constructTree():
+    def nextIndexSplit(symbolsArray):
         pass
 
-treeStr1 = BinaryTree("(((2*(3+2))+5)/2)")
+    def breakupStatement(self):
+        workingInputList = list(self.binaryTreeStr)
+        workinglevelsOfTreeArray = self.levelsOfTree(workingInputList)
+        print(workinglevelsOfTreeArray)
+      
+        if len(workinglevelsOfTreeArray) > 0:
+          lowestVal = len(workinglevelsOfTreeArray)
+          for l in range (0,len(workinglevelsOfTreeArray)):
+            if workinglevelsOfTreeArray[l][1] <= lowestVal:
+              lowestVal = workinglevelsOfTreeArray[l][1]
+        
+        print(lowestVal)
+        
 
-treeStr1.validateString()
+statementOne = BinaryTreeCreator("(((5+2)*(2-1))/((2+9)+((7-2)-1))*8)")
+
+statementOne.breakupStatement()
+(((5+2)*(2-1))/((2+9)+((7-2)-1))*8)
--- scrap.py
+++ scrap.py
@@ -1,3 +1,39 @@
-list =['1','2','3','4','5','6','7','8','9']
-print(list[1])
-print(len(list)-1)
+import copy
+string = '(((5+2)*(2-1))/((2+9)+((7-2)-1))*8)'
+
+openBracketIndexSaver = []
+sortingArray = []
+unsortedIndexArray = []
+completeArray = []
+
+for i in range(0,len(string)):
+    print(string[i])
+    if string[i] == '(':
+        openBracketIndexSaver.append(i)
+        sortingArray.append(i)
+    elif string[i] == ')':
+        print(openBracketIndexSaver)
+        unsortedIndexArray.append( [ openBracketIndexSaver.pop(),i ])
+
+
+for x in range(0,len(sortingArray)):
+    for y in range(0,len(unsortedIndexArray)):
+        if unsortedIndexArray[y][0] == sortingArray[x]:
+            completeArray.append(unsortedIndexArray[y])
+            break
+
+
+
+
+
+
+print(completeArray)
+
+
+print(unsortedIndexArray)
+
+
+
+
+
+# [(1, 35), (2, 14), (3, 7), (9, 13), (16, 32), (17, 21), (23, 31), (24, 28)]
